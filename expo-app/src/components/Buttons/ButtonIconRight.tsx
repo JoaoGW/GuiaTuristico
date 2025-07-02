4b9aacc3f2dafdeaf68e6ba00a5d9114f8705617
JoaoPedro <cpsenha@gmail.com>
2025-07-01 22:27:26 -0300

    Implementação completa do mapa e do botão de Voltar, conteúdo preparado para receber agora o Slide contendo a lista de locais próximos e também os markers no mapa.

--- expo-app/src/components/Buttons/ButtonIconRight.tsx
+++ expo-app/src/components/Buttons/ButtonIconRight.tsx
@@ -9,13 +9,15 @@ import {
 import { ArrowLeft } from "lucide-react-native";
 
 type ButtonContent = {
-  textContent: string
+  textContent: string,
+  action: () => void,
+  styles?: Object
 }
 
-export function ButtonIconRight({ textContent }: ButtonContent) {
+export function ButtonIconRight({ textContent, action, styles }: ButtonContent) {
   return (
-    <ButtonGroup alignItems="center">
-      <Button variant="link">
+    <ButtonGroup alignItems="center" style={ styles }>
+      <Button variant="link" onPress={ action }>
         <ButtonIcon as={ ArrowLeft } size="xl" color="$black" mr={8}/>
         <ButtonText color="$black" size="lg">{ textContent }</ButtonText>
       </Button>
--- expo-app/src/screens/MapsExpanded.tsx
+++ expo-app/src/screens/MapsExpanded.tsx
@@ -1,15 +1,32 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { View } from "@gluestack-ui/themed";
 
 import { ButtonIconRight } from "@components/Buttons/ButtonIconRight";
 import { Maps } from "@components/Maps/Maps";
 
+import { AuthNavigationProp } from "@routes/auth.routes";
+
 export function MapsExpanded() {
+  const navigation = useNavigation<AuthNavigationProp>();
+  
   return (
-    <View px={20} py={20}>
-      <View>
-        <ButtonIconRight textContent="Voltar" />
-      </View>
-      <View>
+    <View style={{ flex: 1, position: 'relative' }}>
+      <ButtonIconRight 
+        textContent="Voltar" 
+        action={ () => navigation.goBack() } 
+        styles={{ 
+          position: 'absolute', 
+          backgroundColor: "#FDFDFD", 
+          borderRadius: 15,
+          paddingVertical: 5,
+          paddingHorizontal: 10,
+          top: 10,
+          left: 10,
+          zIndex: 1
+        }}
+      />
+      <View style={{ flex: 1 }}>
         <Maps />
       </View>
     </View>
